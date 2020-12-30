72284a0f53839b618ee4faf4e195c9dd58e95692
Recovic <recovic@outlook.com>
2020-12-30 19:27:50 +0800

    production

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: process.env.VUE_APP_SERVER_ADDR,
     timeout: 1000 * 5,
     headers: { 'Authorization': 'Bearer ' + localStorage.token }
 });
