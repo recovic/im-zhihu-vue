64b1633159a8095ee5407fff882e75d63e217529
recovic <recovic@outlook.com>
2021-06-03 18:58:34 +0800

    upvote and downvote

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -6,7 +6,7 @@
           <h3>{{ question.title }}</h3>
           {{ question.content }}
           <hr>
-          <b-link :href="'/publishAnswer/'+qid">添加回答</b-link>
+          <b-link class="ml-4" :href="'/publishAnswer/'+qid">添加回答</b-link>
         </b-card>
 
         <b-card no-body class="mt-3">
@@ -17,9 +17,9 @@
             <b-list-group-item href="#" v-for="answer in answers"
                                v-bind:key="answer.id">
               {{ answer.content }}
-              <div class="text-gray">
-                {{ answer.upvote_count }} 次赞同
-              </div>
+              <hr>
+              <b-button variant="outline-primary" size="sm" :pressed="answer.upPressed" @click="upvote(answer)"><b-icon icon="caret-up-fill"></b-icon>{{ answer.upvote_count }} 赞同</b-button>
+              <b-button class="ml-2" variant="outline-primary" size="sm" :pressed="answer.downPressed" @click="downvote(answer)"><b-icon icon="caret-down-fill"></b-icon></b-button>
             </b-list-group-item>
             <b-button v-if="hasMore" variant="outline-primary" @click="getAnswers">加载更多</b-button>
           </b-list-group>
@@ -69,12 +69,52 @@ export default {
                 '&size=15' + '&orderby=' + ctx.order)
                 .then((res) => {
                     if (res.data.code === 0) {
+                        let begin = ctx.answers.length;
                         ctx.answers.push.apply(ctx.answers, res.data.data.answers);
+                        for (let i = begin; i < ctx.answers.length; i++) {
+                            request.get('/vote/answer/' + ctx.answers[i].id)
+                                .then(function (res) {
+                                    if (res.data.code === 0) {
+                                        let s = parseInt(res.data.data);
+                                        ctx.$set(ctx.answers[i], "upPressed", Boolean(s & 1));
+                                        ctx.$set(ctx.answers[i], "downPressed", Boolean(s >> 1 & 1));
+                                    }
+                                });
+                        }
                         ctx.cursor = res.data.data.next_cursor;
                     } else {
                         ctx.hasMore = false;
                     }
-                })
+                });
+
+        },
+        setVote(answer, i) {
+            request.put('/vote/answer/' + answer.id + '?s=' + i)
+                .then(function (res) {
+                    if (res.data.code === 0) {
+                        answer.upPressed = Boolean(i & 1);
+                        answer.downPressed = Boolean(i >> 1 & 1);
+                    }
+                });
+        },
+        upvote(answer) {
+            if (!answer.upPressed) {
+                this.setVote(answer, 1);
+                answer.upvote_count++;
+            } else {
+                this.setVote(answer, 0);
+                answer.upvote_count--;
+            }
+        },
+        downvote(answer) {
+            if (answer.upPressed) {
+                answer.upvote_count--;
+            }
+            if (!answer.downPressed) {
+                this.setVote(answer, 2);
+            } else {
+                this.setVote(answer, 0);
+            }
         }
     }
 }
